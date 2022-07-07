f95494f3be5b1723ce1619e6cbedd28caf2c75d2
Yassin <yassinouider@gmail.com>
2022-07-07 09:38:40 +0200

    disabled if already vote and dont show result if no votes

--- src/pages/poll.js
+++ src/pages/poll.js
@@ -20,16 +20,23 @@ export default function Poll() {
 
   const [isLoading, setIsLoading] = React.useState(false);
   const [answer, setAnswer] = React.useState("");
+  const [hasVote, setHasvote] = React.useState(false);
 
   React.useEffect(() => {
     setAnswer("");
-    if (question.optionOne.votes.find((el) => el === authedUser.id))
+    if (question.optionOne.votes.find((el) => el === authedUser.id)) {
+      setHasvote(true);
       setAnswer(question.optionOne.text);
-    if (question.optionTwo.votes.find((el) => el === authedUser.id))
+    }
+    if (question.optionTwo.votes.find((el) => el === authedUser.id)) {
+      setHasvote(true);
       setAnswer(question.optionTwo.text);
+    }
   }, [authedUser, question]);
 
   const handleAnswer = async (answer) => {
+    if (hasVote) return;
+
     setIsLoading(true);
     await dispatch(
       saveQuestionAnswer({
@@ -67,7 +74,7 @@ export default function Poll() {
             <button
               onClick={() => handleAnswer("optionOne")}
               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 disabled:opacity-50"
-              disabled={isLoading}
+              disabled={isLoading || hasVote}
             >
               {question.optionOne.text}
             </button>
@@ -76,33 +83,37 @@ export default function Poll() {
             <button
               onClick={() => handleAnswer("optionTwo")}
               className="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 disabled:opacity-50"
-              disabled={isLoading}
+              disabled={isLoading || hasVote}
             >
               {question.optionTwo.text}
             </button>
           </div>
         </div>
-        <div className="mt-8">
-          <p className="mb-2">
-            {question.optionOne.votes.length} users (
-            {getPercentage(
-              question.optionOne.votes.length + question.optionTwo.votes.length,
-              question.optionOne.votes.length
-            )}
-            %) prefer{" "}
-            <span className="text-gray-500">{question.optionOne.text}</span>
-          </p>
-          <p>
-            {question.optionTwo.votes.length} users (
-            {getPercentage(
-              question.optionOne.votes.length + question.optionTwo.votes.length,
-              question.optionTwo.votes.length
-            )}
-            %) prefer{" "}
-            <span className="text-gray-500">{question.optionTwo.text}</span>
-          </p>
-        </div>
-
+        {(question.optionOne.votes.length > 0 ||
+          question.optionTwo.votes.length > 0) && (
+          <div className="mt-8">
+            <p className="mb-2">
+              {question.optionOne.votes.length} users (
+              {getPercentage(
+                question.optionOne.votes.length +
+                  question.optionTwo.votes.length,
+                question.optionOne.votes.length
+              )}
+              %) prefer{" "}
+              <span className="text-gray-500">{question.optionOne.text}</span>
+            </p>
+            <p>
+              {question.optionTwo.votes.length} users (
+              {getPercentage(
+                question.optionOne.votes.length +
+                  question.optionTwo.votes.length,
+                question.optionTwo.votes.length
+              )}
+              %) prefer{" "}
+              <span className="text-gray-500">{question.optionTwo.text}</span>
+            </p>
+          </div>
+        )}
         {answer && (
           <div className="mt-8">
             <p>
